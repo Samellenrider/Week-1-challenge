de07696b878ade56f446e03672416b4bfd4b36c1
Frederick Ofori <FrederickCodes@gmail.com>
2017-08-02 15:51:50 +0100

    Making Docking Station release bike

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,12 +1,12 @@
 class DockingStation
-def release_bike
-
-end
+  def release_bike
+    Bike.new
+  end
 end
 
 
 class Bike
-def working?
+  def working?
 
-end
+  end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,4 +2,15 @@ require "docking_station.rb"
 
 describe DockingStation do
   it {expect(subject).to respond_to :release_bike}
- end
+  end
+
+describe DockingStation do
+  it "gets a bike" do
+    bike = subject.release_bike
+    expect(bike).to be_truthy
+
+
+  #it "expects the bike to be working" do
+  #end
+end
+end
